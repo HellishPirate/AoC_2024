313135cf20ff55eebe91bc1786c6bcd2de38945d
HellishPirate <dannyjoseph001@gmail.com>
2024-12-01 16:47:13 +0000

    Update day1.py
    cleaning up

--- day1.py
+++ day1.py
@@ -4,7 +4,6 @@ Created on Sun Dec  1 15:23:45 2024
 
 @author: diana
 """
-print("\033[2J\033[H", end="", flush=True)
 
 import numpy as np
 
@@ -19,9 +18,6 @@ for i in range(len(day1input)):
     list1Min = np.nanargmin(day1input[:,0])
     list2Min = np.nanargmin(day1input[:,1])
     
-    # list1Min = np.where(day1input[:,0] == np.min(day1input[:,0]))
-    # list2Min = np.where(day1input[:,1] == np.min(day1input[:,1]))
-    
     sortedInput[i, 0] = day1input[list1Min,0]
     sortedInput[i, 1] = day1input[list2Min,1]
     
@@ -29,11 +25,10 @@ for i in range(len(day1input)):
     
     day1input[list1Min,0] = np.nan
     day1input[list2Min,1] = np.nan
-    
-    
+     
 totalDistance = np.sum(distances)
 
-test = np.sum((sortedInput[1,0] == sortedInput[:,1]))
 for i in range(len(day1input)):
     similarityScore[i] = sortedInput[i,0] * np.sum((sortedInput[i,0] == sortedInput[:,1]))
+    
 totalSimilarity = np.sum(similarityScore)
