bd26807b61fe84a135bf9d1a53670b60fc2865eb
HellishPirate <dannyjoseph001@gmail.com>
2024-12-03 17:54:35 +0000

    Update day2.py

--- day2.py
+++ day2.py
@@ -1,3 +1,4 @@
+print("\033[2J\033[H", end="", flush=True)
 # -*- coding: utf-8 -*-
 """
 Created on Mon Dec  2 13:11:25 2024
@@ -16,14 +17,14 @@ for i in range(len(day2input)):
     entry = np.fromstring(day2input[i], dtype=int, sep=" ")
     differences = np.zeros((len(entry)-1,))
     _, counts = np.unique(entry, return_counts=True)
-    test = (entry == np.sort(entry))
-    test1 = (entry == np.flip(np.sort(entry)))
+    test = all(entry == np.sort(entry))
+    test1 = all(entry == np.flip(np.sort(entry)))
     test2 = (sum(counts) == len(counts))
-    if (all(test) or all(test1)) and test2 == True:
+    if (test or test1) and test2:
         for j in range(len(entry)-1):
             differences[j] = np.abs(entry[j] - entry[j+1])
-        test3 = differences <= 3 
-        if all(test3):
+        test3 = all(differences <= 3)
+        if test3:
             safeCountPart1 = safeCountPart1 + 1
             safeRows[i] = 1
             
@@ -37,14 +38,14 @@ for i in range(len(day2input)):
         entry1 = np.delete(entry, j)
         differences = np.zeros((len(entry1)-1,))
         _, counts = np.unique(entry1, return_counts=True)
-        test = (entry1 == np.sort(entry1))
-        test1 = (entry1 == np.flip(np.sort(entry1)))
+        test = all(entry1 == np.sort(entry1))
+        test1 = all(entry1 == np.flip(np.sort(entry1)))
         test2 = (sum(counts) == len(counts))
-        if (all(test) or all(test1)) and test2 == True and not safeRows[i] and not safeFlag:
+        if (test or test1) and test2 and not safeRows[i] and not safeFlag:
             for k in range(len(entry1)-1):
                 differences[k] = np.abs(entry1[k] - entry1[k+1])
-            test3 = differences <= 3 
-            if all(test3):
+            test3 = all(differences <= 3) 
+            if test3:
                 safeCountPart2 = safeCountPart2 + 1
                 safeFlag = 1
 
